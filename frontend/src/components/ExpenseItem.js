f5f5a762a2a56e4ff3fd4650c7b0d08b1b0979f9
Ankush <ankushsharma9685@gmail.com>
2024-01-31 19:26:48 +0530

    added stylesheet & LocationOfExpenditure

--- frontend/src/components/ExpenseItem.js
+++ frontend/src/components/ExpenseItem.js
@@ -1,13 +1,20 @@
-const expenses = [{ name: "Food", price: 100 },
-{ name: "Petrol", price: 100 },
-{ name: "Movies", price: 100 }]
+import "../styles/expenseItem.css"
+
+const expenses = [{ name: "Food", price: 100, LocationOfExpenditure: "movie" },
+{ name: "Petrol", price: 100, LocationOfExpenditure: "movie" },
+{ name: "Movies", price: 100, LocationOfExpenditure: "movie" }]
 
 const ExpenseItem = () => {
     return (
         <>
             <h1>Expense Items</h1>
             <ul>
-                {expenses.map(item => <li>{`${item.name} Rs ${item.price}`}</li>)}
+                {expenses.map(item =>
+                    <li className="expense-item">
+                        <p>{item.name} </p>
+                        <p className="expense-item__LocationOfExpenditureription">{item.LocationOfExpenditure}</p>
+                        <p className="expense-item__price">Rs {item.price}</p>
+                    </li>)}
             </ul>
         </>)
 }

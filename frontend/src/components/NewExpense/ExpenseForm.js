8ed19c71c1fbd3f5e181a09b5985484393059863
Ankush <ankushsharma9685@gmail.com>
2024-02-01 15:07:02 +0530

    handled adding of expense

--- frontend/src/components/NewExpense/ExpenseForm.js
+++ frontend/src/components/NewExpense/ExpenseForm.js
@@ -3,23 +3,34 @@ import { useState } from "react";
 const ExpenseForm = ({ SetAllExpense }) => {
     const [expense, setExpense] = useState({
         name: "",
-        price: null,
-        date: new Date()
+        price: "",
+        date: "",
     });
 
     const handleInputChange = (e) => {
-        setExpense({ ...expense, [e.target.name]: e.target.value });
+        let { name, value } = e.target;
+        setExpense({ ...expense, [name]: value });
+    }
+
+    const handleAddExpense = (e) => {
+        e.preventDefault();
+        console.log(expense)
+        setExpense({
+            name: "",
+            price: "",
+            date: "",
+        })
     }
 
     return (
-        <form>
+        <form onSubmit={handleAddExpense} id="addExpenseForm">
             <label htmlFor="name">Title</label>
             <input id="name" name="name" value={expense.name} onChange={handleInputChange}></input>
             <label htmlFor="amount">Amount</label>
             <input id="amount" name="price" type="number" min={0.00} value={expense.price} onChange={handleInputChange}></input>
             <label htmlFor="date">Date</label>
-            <input id="date" name="date" type="date" value={expense.date} onChange={handleInputChange}></input>
-            <button > AddExpense</button>
+            <input id="date" name="date" type="date" value={expense.date.toLocaleString()} onChange={handleInputChange}></input>
+            <button type="submit"> AddExpense</button>
         </form>
     )
 }

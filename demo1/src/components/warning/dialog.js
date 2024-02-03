a9f21134c8fc59ad0f4501ab48cf6d21fd9f4973
Ankush <ankushsharma9685@gmail.com>
2024-02-03 16:03:02 +0530

    added portal to show modal on top

--- demo1/src/components/warning/dialog.js
+++ demo1/src/components/warning/dialog.js
@@ -1,20 +1,35 @@
 import "../../styles/warning/dialog.css"
+import ReactDOM from "react-dom"
 
-const Dialog = ({ head, content, onChangeDialog }) => {
+const Backdrop = props => {
+    return <div className="backdrop" onClick={props.onConfirm}></div>
+}
+
+const ModalOverlay = (props) => {
     return (
         <div className="dialog">
             <div className="dialog-head">
-                <h1>{head}</h1>
+                <h1>{props.head}</h1>
             </div>
             <div className="dialog-body">
                 <div className="dialog-content">
-                    <p>{content}</p>
+                    <p>{props.content}</p>
                 </div>
                 <div className="dialog-action">
-                    <button onClick={onChangeDialog}>Okay</button>
+                    <button onClick={props.onConfirm}>Okay</button>
                 </div>
             </div>
         </div>)
+}
+
+const Dialog = (props) => {
+    return (
+        <>
+            {/**createPortal(element,place where to render) */}
+            {ReactDOM.createPortal(<Backdrop onConfirm={props.onChangeDialog} />, document.getElementById("backdrop-root"))}
+            {ReactDOM.createPortal(<ModalOverlay head={props.head} content={props.content} onConfirm={props.onChangeDialog} />, document.getElementById("overlay-root"))}
+        </>
+    )
 }
 
 export default Dialog;

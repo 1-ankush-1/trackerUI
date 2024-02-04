e8a321953a90efc1680cb49545848b0f7beb5896
Ankush <ankushsharma9685@gmail.com>
2024-02-04 17:33:52 +0530

    consume data with useContext hook

--- demo-2/src/store/auth-context.js
+++ demo-2/src/store/auth-context.js
@@ -3,7 +3,9 @@ import React from 'react'
 
 //create context which return a object that contain component
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    //for autocompletion suggestion
+    onLogout: () => {}
 })
 
 export default AuthContext;

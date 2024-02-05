7495ad2d5288c94cb7ab54874330bd21eda0f72a
Ankush <ankushsharma9685@gmail.com>
2024-02-05 21:05:42 +0530

    create order added

---
+++ proj1/frontend/src/components/createOrder/CreateForm.js
@@ -0,0 +1,76 @@
+import { useState, useContext } from "react";
+import { orderContext } from "../../store/order";
+import "../../styles/createOrder/createForm.css"
+
+const CreateForm = () => {
+    const ordCtx = useContext(orderContext);
+
+    const [order, setOrder] = useState({
+        orderId: "",
+        dish: "",
+        price: "",
+        table: ""
+    })
+
+    const onChangeOrderHandler = (e) => {
+        setOrder({ ...order, [e.target.name]: e.target.value });
+    }
+
+    const handleOrderForm = (e) => {
+        e.preventDefault();
+
+        if (order.orderId === "" || order.dish === "" || order.price === "" || order.table === "") {
+            alert("one or more missing field");
+            return;
+        }
+
+        const exist = ordCtx.orders.filter((odr) => {
+            return odr.orderId === order.orderId;
+        })
+
+        if (exist.length > 0) {
+            alert("id already registered Enter unique id");
+            return
+        }
+
+        ordCtx.onOrderChange(order);
+    }
+
+    return (
+        <form id="order-form" onSubmit={handleOrderForm}>
+            <label htmlFor="order_id" className="order-lbl">Unique Order Id</label>
+            <input
+                className="order-inp"
+                id="order_id"
+                name="orderId"
+                type="number"
+                onChange={onChangeOrderHandler}
+            />
+            <label htmlFor="order_price" className="order-lbl">Choose Price</label>
+            <input
+                className="order-inp"
+                id="order_price"
+                type="number"
+                name="price"
+                onChange={onChangeOrderHandler}
+            />
+            <label htmlFor="order_dish" className="order-lbl">Choose Dish</label>
+            <input
+                className="order-inp"
+                id="order_dish"
+                name="dish"
+                type="text"
+                onChange={onChangeOrderHandler}
+            />
+            <label htmlFor="order_table" className="order-lbl">Choose Table</label>
+            <select id="order_table" defaultValue={"table1"} name="table" onChange={onChangeOrderHandler} className="order-form-select">
+                <option value={"table1"}>Table 1</option>
+                <option value={"table2"}>Table 2</option>
+                <option value={"table3"}>Table 3</option>
+            </select>
+            <button type="submit" className="order-btn">Add to bill</button>
+        </form>
+    )
+}
+
+export default CreateForm;
---
+++ proj1/frontend/src/components/createOrder/CreateOrder.js
@@ -0,0 +1,12 @@
+import CreateForm from "./CreateForm";
+import "../../styles/createOrder/createOrder.css"
+
+const CreateOrder = () => {
+    return (
+        <div className="form-parent">
+            <CreateForm />
+        </div>
+    )
+}
+
+export default CreateOrder;

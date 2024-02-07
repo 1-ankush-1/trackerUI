f83703690211123554c9868b1fbd3f1ccb29d6f5
Ankush <ankushsharma9685@gmail.com>
2024-02-07 16:10:52 +0530

    added meal summary

--- Restaurant/src/App.js
+++ Restaurant/src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Layout/Header"
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <Meals />
     </>
   );
 }

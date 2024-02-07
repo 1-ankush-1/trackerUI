1e0c2780b13f2aef65c04eaa604dd8d10915dde1
Ankush <ankushsharma9685@gmail.com>
2024-02-07 18:05:24 +0530

    added modal for cart

--- Restaurant/src/Components/Cart/Cart.js
+++ Restaurant/src/Components/Cart/Cart.js
@@ -1,16 +1,21 @@
 import "../../Styles/Cart/Cart.css"
+import Modal from "../UI/Model";
 
 const Cart = () => {
+
     return (
-        <div className="cart">
-            <div>
-                <img src="" alt="cart-icon"></img>
+        <>
+            <div className="cart">
+                <div>
+                    <img src="" alt="cart-icon"></img>
+                </div>
+                <div>
+                    <p>your cart</p>
+                </div>
+                <div>{0}</div>
             </div>
-            <div>
-                <p>your cart</p>
-            </div>
-            <div>{0}</div>
-        </div>
+            <Modal />
+        </>
     )
 }
 
--- Restaurant/src/Components/UI/Model.js
+++ Restaurant/src/Components/UI/Model.js
@@ -1,17 +1,38 @@
 import React from "react"
+import ReactDom from "react-dom";
+import "../../Styles/UI/Modal.css"
 
-const Backdrop = (props) => {
-    return <></>
+
+const ModalItems = (props) => {
+    return (<div>
+        <div>{props.name}</div>
+    </div>)
 }
 
-const ModalOverlay = () => {
-    return <></>
+const Backdrop = () => {
+    return <div className="backdrop"></div>
+}
+
+const ModalOverlay = (props) => {
+    return (
+        <div className="modal-overlay">
+            <ModalItems name={"some"} />
+            <div className="total-amt">
+                <h2>Total Amount</h2>
+                <h2>{`₹12`}</h2>
+            </div>
+            <div className="modal-btn-components">
+                <button className="close-btn">close</button>
+                <button className="order-btn">order</button>
+            </div>
+        </div>
+    )
 }
 
 const Modal = (props) => {
     return (<>
-        {React.createPortal(<Backdrop />, document.getElementById("cart-backdrop"))}
-        {React.createPortal(<ModalOverlay />, document.getElementById("cart-modal"))}
+        {ReactDom.createPortal(<Backdrop />, document.getElementById("cart-backdrop"))}
+        {ReactDom.createPortal(<ModalOverlay />, document.getElementById("cart-modal"))}
     </>)
 }
 

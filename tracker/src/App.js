b7722d942bcdd8eea75b2d8aade14b197fc150d6
Ankush <ankushsharma9685@gmail.com>
2024-03-04 13:31:59 +0530

    created basic layout

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Footer from './Layout/Footer/Footer';
+import Header from './Layout/Header/Header';
+import SignUp from './Pages/AuthPages/SignUp';
 
 function App() {
   return (
-    <div >
-      tracker
-    </div>
+    <>
+      <Header />
+      <main>
+        <SignUp />
+      </main>
+      <Footer />
+    </>
   );
 }
 

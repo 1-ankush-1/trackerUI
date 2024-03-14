9536c2f076e5129c17d391754e2c433982c852a0
Ankush <ankushsharma9685@gmail.com>
2024-03-14 23:17:22 +0530

    change logic according to login and logout of authSlice

--- redux-w-react/src/App.js
+++ redux-w-react/src/App.js
@@ -1,14 +1,19 @@
+import { useSelector } from 'react-redux';
 import Auth from './components/Auth';
 import Counter from './components/Counter';
 import Header from './components/Header';
 
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   return (
     <>
-      <Header />
-      <Auth />
-      <Counter />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated &&
+        <>
+          <Header />
+          <Counter />
+        </>}
     </>
   );
 }
--- redux-w-react/src/components/Auth.js
+++ redux-w-react/src/components/Auth.js
@@ -1,6 +1,14 @@
+import { useDispatch } from 'react-redux';
 import classes from './Auth.module.css';
+import { authAction } from '../store';
 
 const Auth = () => {
+  const dispatch = useDispatch();
+
+  const handelUserInput = () => {
+    dispatch(authAction.login());
+  }
+
   return (
     <main className={classes.auth}>
       <section>
@@ -13,7 +21,7 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button>Login</button>
+          <button onClick={handelUserInput}>Login</button>
         </form>
       </section>
     </main>

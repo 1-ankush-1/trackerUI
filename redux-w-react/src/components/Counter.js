5b93bce9aab714baa9dfe3fbae6b988e84cd9782
Ankush <ankushsharma9685@gmail.com>
2024-03-14 20:29:56 +0530

    get data from the counter slice

--- redux-w-react/src/components/Counter.js
+++ redux-w-react/src/components/Counter.js
@@ -1,12 +1,15 @@
 import classes from './Counter.module.css';
-
+import { UseSelector, useDispatch, useSelector } from 'react-redux';
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  //to get particular store slice state data
+  const counter = useSelector((state) => state.counter);
+
+  const toggleCounterHandler = () => { };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );

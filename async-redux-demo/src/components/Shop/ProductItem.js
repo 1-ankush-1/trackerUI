6e13763111e76854e7ba0434d6805b43ec8dd19b
Ankush <ankushsharma9685@gmail.com>
2024-03-17 14:11:21 +0530

    added action in product add handler

--- async-redux-demo/src/components/Shop/ProductItem.js
+++ async-redux-demo/src/components/Shop/ProductItem.js
@@ -1,8 +1,16 @@
+import { useDispatch } from 'react-redux';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
+import { cartAction } from '../../redux/slices/cart';
 
 const ProductItem = (props) => {
   const { title, price, description } = props;
+  const dispatch = useDispatch();
+
+  const handleAddItemsInCart = () => {
+    const id = Math.random().toString(36).substr(2, 9);
+    dispatch(cartAction.add({ id: id, title: title, price: price, description: description }));
+  }
 
   return (
     <li className={classes.item}>
@@ -13,7 +21,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={handleAddItemsInCart}>Add to Cart</button>
         </div>
       </Card>
     </li>
--- async-redux-demo/src/components/Shop/Products.js
+++ async-redux-demo/src/components/Shop/Products.js
@@ -11,6 +11,11 @@ const Products = (props) => {
           price={6}
           description='This is a first product - amazing!'
         />
+        <ProductItem
+          title='Test2'
+          price={44}
+          description='This is a first product - great!'
+        />
       </ul>
     </section>
   );

a1e24db118ad5a828166f59bf5f2a65c5411ce36
Ankush <ankushsharma9685@gmail.com>
2024-03-17 14:10:50 +0530

    show cart items and total amount

--- async-redux-demo/src/components/Cart/Cart.js
+++ async-redux-demo/src/components/Cart/Cart.js
@@ -1,16 +1,28 @@
+import { useSelector } from 'react-redux';
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
 const Cart = (props) => {
+  const cartItems = useSelector(state => state.cart.items);
+  const totalAmount = useSelector(state => state.cart.totalPrice);
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
-      <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
-      </ul>
+      {cartItems.length > 0 &&
+        <>
+          <ul>
+            {cartItems.map((item, i) => {
+              return <CartItem
+                key={i}
+                item={item}
+              />
+            })}
+          </ul>
+          <h2>totalAmount : {totalAmount}</h2>
+        </>
+      }
+      {cartItems.length === 0 && <h2>no items in cart ...</h2>}
     </Card>
   );
 };
--- async-redux-demo/src/components/Cart/CartItem.js
+++ async-redux-demo/src/components/Cart/CartItem.js
@@ -1,14 +1,23 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartItem.module.css';
+import { cartAction } from '../../redux/slices/cart';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch()
+  const { id, title, quantity, total, price } = props.item;
+  const handleIncrementItemQuantity = () => {
+    dispatch(cartAction.add({ quantity: quantity + 1, id, price }));
+  }
+  const handleDecrementItemQuantity = () => {
+    dispatch(cartAction.remove(id));
+  }
 
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${quantity * price.toFixed(2)}{' '}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -17,8 +26,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={handleDecrementItemQuantity}>-</button>
+          <button onClick={handleIncrementItemQuantity}>+</button>
         </div>
       </div>
     </li>

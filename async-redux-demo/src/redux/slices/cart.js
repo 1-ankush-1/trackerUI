0a0c99ef6078d9ac8eef358dc7dd5eeabc132470
Ankush <ankushsharma9685@gmail.com>
2024-03-17 14:09:39 +0530

    added actions for cart

--- async-redux-demo/src/redux/slices/cart.js
+++ async-redux-demo/src/redux/slices/cart.js
@@ -1,12 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const cartInitialState = [];
+const cartInitialState = { items: [], totalQuantity: 0, totalPrice: 0 };
 
 const cartSlice = createSlice({
     name: "cart",
     initialState: cartInitialState,
     reducers: {
-
+        add: (state, action) => {
+            const item = action.payload;
+            const existingItem = state.items.find((itemExist) => itemExist.id === item.id);
+            if (!existingItem) {
+                state.items.push({
+                    id: item.id,
+                    title: item.title,
+                    price: item.price,
+                    description: item.description,
+                    quantity: 1,
+                });
+            } else {
+                existingItem.quantity += 1;
+            }
+            state.totalQuantity += 1;
+            state.totalPrice += item.price;
+        },
+        remove: (state, action) => {
+            const id = action.payload;
+            const existingItem = state.items.find(itemExist => itemExist.id === id);
+            if (existingItem.quantity > 1) {
+                existingItem.quantity -= 1;
+            } else {
+                state.items = state.items.filter(item => item.id !== id);
+            }
+            state.totalQuantity -= 1;
+            state.totalPrice -= existingItem.price;
+        }
     }
 })
 

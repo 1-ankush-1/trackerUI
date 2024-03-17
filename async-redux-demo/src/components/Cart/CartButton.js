029679596794d50cf12ea2324876b9292cf2f7f4
Ankush <ankushsharma9685@gmail.com>
2024-03-17 13:16:33 +0530

    added logic to toggle cartUI

--- async-redux-demo/src/components/Cart/CartButton.js
+++ async-redux-demo/src/components/Cart/CartButton.js
@@ -1,8 +1,14 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartButton.module.css';
+import { cartUIAction } from '../../redux/slices/cartUI';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const handleCartVisibility = () => {
+    dispatch(cartUIAction.toggle());
+  }
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={handleCartVisibility}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>

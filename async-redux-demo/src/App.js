1684c8f6ac4c6ada915be21bf267f1c9183fab02
Ankush <ankushsharma9685@gmail.com>
2024-03-18 19:41:17 +0530

    added useEffect to update data in firebase with cart

--- async-redux-demo/src/App.js
+++ async-redux-demo/src/App.js
@@ -2,9 +2,24 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
 
 function App() {
   const isHidden = useSelector(state => state.cartUI.isHidden);
+  const cart = useSelector(state => state.cart);
+  console.log(cart)
+
+  useEffect(() => {
+    console.log("called");
+    fetch("https://async-react-add96-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  }, [cart])
+
   return (
     <Layout>
       {isHidden && <Cart />}

b2c482f62f8c46e455fe6add05d818a76df087a4
Ankush <ankushsharma9685@gmail.com>
2024-03-19 18:29:09 +0530

    added test suite to combine tests

--- tracker/src/Components/Expenses/AddExpense.test.js
+++ tracker/src/Components/Expenses/AddExpense.test.js
@@ -1,14 +1,18 @@
-import { render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import AddExpense from "./AddExpense";
 
+//test suite - ("description,function which have different test belong to same feature")
+describe("Add Expense component", () => {
+    //test("description",testin function)
+    test('check we are getting component', () => {
+        //Arrange - setup test data ,condition and environment
+        render(<AddExpense />);
+        //Act - run logic that will be tested
 
-//test("description",testin function)
-test('check we are getting data or not', () => {
-    //Arrange - setup test data ,condition and environment
-    render(<AddExpense />);
-    //Act - run logic that will be tested
+        //Assert - compare executed result with expected result
+        const amount = screen.getByText("Amount", { exact: true });  //check if the text exist on the screen and should be exact match
+        expect(amount).toBeInTheDocument();
+    })
+});
 
-    //Assert - compare executed result with expected result
-    const amount = screen.getByText("Amount", { exact: true });  //check if the text exist on the screen and should be exact match
-    expect(amount).toBeInTheDocument();
-})
+

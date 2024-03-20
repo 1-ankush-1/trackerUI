6cb7280d50732076ccf0821ffa5b07fe2e3a2f06
Ankush <ankushsharma9685@gmail.com>
2024-03-20 14:43:26 +0530

    added notification state inside the app component.

--- tracker/src/Routes/Router.js
+++ tracker/src/Routes/Router.js
@@ -1,19 +1,38 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import UserRoutes from "./UserRoutes";
 import AuthRoutes from "./AuthRoutes";
-import { AuthContext } from "../Store/authStore";
-import { useContext } from "react";
-import { useSelector } from "react-redux";
+import {  useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Notification from "../UI/Notification";
+import { notificationAction } from "../redux/slices/notification";
 
 const Router = () => {
     // const authCtx = useContext(AuthContext);
     const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+    const notification = useSelector(state => state.notification);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        function clearNotificationAfterCertainTime() {
+            dispatch(notificationAction.clearNotification());
+        }
+        const timer = setTimeout(() => {
+            clearNotificationAfterCertainTime();
+        }, 5000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [notification.status, dispatch]);
 
     return (
-        <Routes>
-            <Route path="/auth/*" element={!isAuthenticated ? <AuthRoutes /> : <Navigate to="/" replace />} />
-            <Route path="/*" element={isAuthenticated ? <UserRoutes /> : <Navigate to="/auth/login" replace />} />
-        </Routes>
+        <>
+            {notification.status !== "" && <Notification status={notification.status} message={notification.message} title={notification.title} />}
+            <Routes>
+                <Route path="/auth/*" element={!isAuthenticated ? <AuthRoutes /> : <Navigate to="/" replace />} />
+                <Route path="/*" element={isAuthenticated ? <UserRoutes /> : <Navigate to="/auth/login" replace />} />
+            </Routes>
+        </>
     )
 }
 

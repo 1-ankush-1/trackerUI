079d4bfcf18b1c179e67da16c57a8d9837837b34
Ankush <ankushsharma9685@gmail.com>
2024-03-20 13:55:03 +0530

    updated  route logic action creator instead of context

--- tracker/src/Routes/Router.js
+++ tracker/src/Routes/Router.js
@@ -3,13 +3,16 @@ import UserRoutes from "./UserRoutes";
 import AuthRoutes from "./AuthRoutes";
 import { AuthContext } from "../Store/authStore";
 import { useContext } from "react";
+import { useSelector } from "react-redux";
 
 const Router = () => {
-    const authCtx = useContext(AuthContext);
+    // const authCtx = useContext(AuthContext);
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
     return (
         <Routes>
-            <Route path="/auth/*" element={!authCtx.isLoggedIn ? <AuthRoutes /> : <Navigate to="/" replace />} />
-            <Route path="/*" element={authCtx.isLoggedIn ? <UserRoutes /> : <Navigate to="/auth/login" replace />} />
+            <Route path="/auth/*" element={!isAuthenticated ? <AuthRoutes /> : <Navigate to="/" replace />} />
+            <Route path="/*" element={isAuthenticated ? <UserRoutes /> : <Navigate to="/auth/login" replace />} />
         </Routes>
     )
 }

b8252a9f32d9085fa14ef5d281a2856ad597b37c
Ankush <ankushsharma9685@gmail.com>
2024-03-20 14:42:52 +0530

    creaed notification component and added notification slice in root reducer

--- tracker/src/redux/slices/index.js
+++ tracker/src/redux/slices/index.js
@@ -2,12 +2,14 @@ import authReducer from "./auth"
 import userReducer from "./user"
 import expenseReducer from "./expense"
 import themeReducer from "./theme"
+import notificationReducer from "./notification"
 
 const rootReducer = {
     theme: themeReducer,
     auth: authReducer,
     user: userReducer,
-    expense: expenseReducer
+    expense: expenseReducer,
+    notification: notificationReducer
 }
 
 export default rootReducer;

9f1daf6d8b7c0c0e59a9589d3c710c0efb95c941
Ankush <ankushsharma9685@gmail.com>
2024-03-20 14:42:08 +0530

    added notification action inside the auth action creator

--- tracker/src/redux/actions/auth.js
+++ tracker/src/redux/actions/auth.js
@@ -1,13 +1,32 @@
 import authService from "../../Services/authServices";
 import { authAction } from "../slices/auth";
+import { notificationAction } from "../slices/notification";
 
 export function loginUser(user) {
     return async (dispatch) => {
-        const response = await authService.login({ email: user.email, password: user.password });
-        if (response.data) {
-            localStorage.setItem("token", response.data.token)
-            dispatch(authAction.login({ token: response.data.token, isAuthenticated: true }));
-            alert("login");
+        dispatch(notificationAction.showNotification({
+            status: "pending",
+            title: "sending..",
+            message: "Logging in user"
+        }))
+
+        try {
+            const response = await authService.login({ email: user.email, password: user.password });
+            if (response.data) {
+                localStorage.setItem("token", response.data.token)
+                dispatch(authAction.login({ token: response.data.token, isAuthenticated: true }));
+                dispatch(notificationAction.showNotification({
+                    status: "success",
+                    title: "Success",
+                    message: "User Successfully logged in"
+                }))
+            }
+        } catch (error) {
+            dispatch(notificationAction.showNotification({
+                status: "error",
+                title: "Error",
+                message: "failed to authenticate user"
+            }))
         }
     }
 }

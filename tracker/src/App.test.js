dc6b67b8a9769b0f3f16c7f872a83869299df412
Ankush <ankushsharma9685@gmail.com>
2024-03-20 13:09:42 +0530

    added test script for app

--- tracker/src/App.test.js
+++ tracker/src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//mocking firebase function
+jest.mock("firebase/auth", () => ({
+    getAuth: jest.fn(),
+}));
+
+jest.mock("firebase/firestore", () => ({
+    getFirestore: jest.fn(),
+}));
+
+test('render App Component', () => {
+    render(
+        <App />
+    );
 });
